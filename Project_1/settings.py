984aaa1d035493b5c2cbc19c7a9fefdf1edc8774
VENKATESH_CHILUKAMARI <104677113+Venkey2238@users.noreply.github.com>
2022-12-26 13:29:18 +0530

    Update settings.py

--- Project_1/settings.py
+++ Project_1/settings.py
@@ -50,7 +50,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'Project_1.urls'
